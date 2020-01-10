e0aad5a72b6d71eccef564056568f480dfc48f2b
carmona44 <daniel_95ca@hotmail.com>
2020-01-10 20:07:57 +0100

    Añadir vuetify y  configuración inicial

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,36 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
     <v-content>
-      <HelloWorld/>
+      <v-flex xs12>
+        <v-img
+          :src="require('@/assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        ></v-img>
+      </v-flex>
+      <v-tabs fixed-tabs background-color="orange" dark>
+        <v-tab v-for="(tab, index) in tabs" :key="index">
+          {{ tab }}
+        </v-tab>
+      </v-tabs>
+      <KOMClasification />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import KOMClasification from "./components/KOMClasification";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    HelloWorld,
+    KOMClasification
   },
 
   data: () => ({
-    //
-  }),
+    tabs: ["ClasificaKOM", "Segmentos recomendados", "Estadísticas"]
+  })
 };
 </script>
